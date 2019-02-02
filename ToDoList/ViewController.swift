43d3c92b101c1e135f3666ddb0a64fb77639cb98
Guilherme Fernando Moser <moserguilherme@gmail.com>
2019-02-02 19:31:54 -0200

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Guilherme Moser on 02/02/19.
+//  Copyright © 2019 Guilherme Moser. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
